067871f3c59c2c3709dc47d2232d49fa2e1ab6a2
std-freejia <std-freejia@gmail.com>
2021-07-11 17:37:17 +0900

    [파일 업로드] - 예제로 구현하는 파일 업로드, 다운로드
    이미지를 웹브라우저에 보여주는 기능
    첨부파일명 클릭시, 파일 열어서 웹브라우저에 보여주기
    첨부파일명 클릭시, 이미지 파일 다운로드 (파일명 인코딩 유의)

--- upload/src/main/java/hello/upload/controller/ItemController.java
+++ upload/src/main/java/hello/upload/controller/ItemController.java
@@ -6,12 +6,19 @@ import hello.upload.domain.UploadFile;
 import hello.upload.file.FileStore;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.util.UriUtils;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 @Slf4j
@@ -32,13 +39,13 @@ public class ItemController {
                            RedirectAttributes redirectAttributes) throws IOException {
 
         /** 파일 저장 : 특정 경로에 저장  */
-        UploadFile attacheFile = fileStore.storeFile(form.getAttachFile());
+        UploadFile attachFile = fileStore.storeFile(form.getAttachFile());
         List<UploadFile> storeImageFiles = fileStore.storeFiles(form.getImageFiles());
 
         /** DB에 저장 : 파일이 저장된 '상대경로'를 저장. 보통 fullpath를 저장하지는 않음 */
         Item item = new Item();
         item.setItemName(form.getItemName());
-        item.setAttacheFile(attacheFile);
+        item.setAttachFile(attachFile);
         item.setImageFiles(storeImageFiles);
         itemRepository.save(item);
 
@@ -54,4 +61,36 @@ public class ItemController {
         model.addAttribute("item", item);
         return "item-view";
     }
+
+    /** 이미지 보여주기 (보안에 취약한 코드) */
+    @ResponseBody
+    @GetMapping("/images/{filename}")
+    public Resource downloadImage(@PathVariable String filename) throws MalformedURLException {
+        /** url : file:/Users/../image.jpg  */
+        return new UrlResource("file:" + fileStore.getFullPath(filename));
+    }
+
+    /** 첨부파일명 클릭 시, */
+    @GetMapping("/attach/{itemId}")
+    public ResponseEntity<Resource> downloadAttach(@PathVariable Long itemId) throws MalformedURLException {
+        Item item = itemRepository.findById(itemId);
+        /** itemId 를 이용하여 아이템에 따른 접근 권한을 확인하는 로직을 끼워넣어도 좋다. */
+
+        String storeFileName = item.getAttachFile().getStoreFileName(); /** 서버에 저장된 파일명 */
+        String uploadFileName = item.getAttachFile().getUploadFileName(); /** 사용자가 저장한 파일명 */
+
+        UrlResource urlResource = new UrlResource("file:" + fileStore.getFullPath(storeFileName));
+
+        log.info("uploadFileName={}", uploadFileName);
+
+        /** 한글 및 특수문자 등이 깨지지 않도록 파일명 인코딩 */
+        String encodedUploadFileName = UriUtils.encode(uploadFileName, StandardCharsets.UTF_8);
+        /** 파일을 다운로드 받을 수 있도록 CONTENT_DISPOSITION */
+        String contentDisposition = "attachment; filename=\"" + encodedUploadFileName + "\"";
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, contentDisposition)
+                .body(urlResource);
+    }
+
 }
--- upload/src/main/java/hello/upload/domain/Item.java
+++ upload/src/main/java/hello/upload/domain/Item.java
@@ -9,6 +9,6 @@ public class Item {
 
     private Long id;
     private String itemName;
-    private UploadFile attacheFile;
+    private UploadFile attachFile;
     private List<UploadFile> imageFiles; // 파일목록
 }
