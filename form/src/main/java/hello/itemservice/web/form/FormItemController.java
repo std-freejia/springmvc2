b2eb839df320f095b260f3d9489165ac896862f3
std-freejia <std-freejia@gmail.com>
2021-07-17 01:44:07 +0900

    [타임리프 - 스프링 통합과 폼] 멀티 체크박스
    자주 사용되는 Model 을 담아 보내는 기능
    @ModelAttribute("regions")
    
    동적으로 id가 생성되어야 하는 경우,
    ids.prev 또는 ids.next를 이용하여 id를 달리하여 생성해준다.
    
    LinkedHashMap에 체크박스 항목리스트를 담아 Model로 전송한다.
    th:field 와, th:value 리스트값을 비교하여, 사용자가 체크한 것만 체크하여 보여준다.

--- form/src/main/java/hello/itemservice/web/form/FormItemController.java
+++ form/src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -9,7 +9,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -18,6 +20,17 @@ import java.util.List;
 public class FormItemController {
 
     private final ItemRepository itemRepository;
+
+    /** 모든 요청에 담기는 모델 정의.  해당 컨트롤러의 모든 메소드를 호출할 때, regions 이름의 모델이 담긴다.  */
+    @ModelAttribute("regions")
+    public Map<String, String> regions(){
+        Map<String, String> regions = new LinkedHashMap<>(); // 순서가 보장되는 LinkedHashMap
+        regions.put("SEOUL", "서울"); // {키, 사용자에게 보여주는 값}
+        regions.put("BUSAN", "부산");
+        regions.put("JEJU", "제주");
+        return regions;
+        // 자주 쓰이는 모델을 쓸 때 @ModelAttribute("regions") 사용.
+    }
 
     @GetMapping
     public String items(Model model) {
@@ -38,12 +51,15 @@ public class FormItemController {
     {
         // 빈 모델 넘겨주기
         model.addAttribute("item", new Item());
+
         return "form/addForm";
     }
 
     @PostMapping("/add")
     public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
         log.info("item.getOpen={}", item.getOpen());
+        log.info("item.regions={}", item.getRegions()); // 등록 지역 리스트
+
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
