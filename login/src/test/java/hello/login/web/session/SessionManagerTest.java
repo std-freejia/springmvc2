dcfe1bbb110c883114c70a611fa77004fddbd321
std-freejia <wtfreejia@gmail.com>
2021-09-17 01:55:34 +0900

    [로그인 처리1 - 쿠키, 세션] 로그인 처리하기 - 세션 동작 방식, 세션 직접 만들기.
    [세션 동작 방식 ]
    1. 중요 정보를 모두 서버에 저장.
    클라이언트와 서버가 주고받는 쿠키를 추정 불가능한 식별자 값으로 연결해야 한다!
    
    2. 서버에 '세션 저장소'를 관리한다.
    로그인 성공 시, UUID로 랜덤 토큰을 만든다.
    {sessionId : 토큰값 , value: memberA }
    
    3. 브라우저에 쿠키를 보내되, 쿠키값을 토큰값으로 넘긴다.
    
    4. 브라우저는 토큰값을 쿠키저장소에 보관한다.
    클라이언트와 서버는 서로 추정불가능한 토큰값만 쿠키를 통해 주고받는다!
    ----------
    [세션 기능 ]
    세션 아이디(임의의 토큰값) 만들기
    세션 아이디로 멤버 정보 조회
    세션 만료

---
+++ login/src/test/java/hello/login/web/session/SessionManagerTest.java
@@ -0,0 +1,36 @@
+package hello.login.web.session;
+
+import hello.login.domain.member.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SessionManagerTest {
+
+    SessionManager sessionManager = new SessionManager();
+
+    /**
+     * HttpServletResponse 는 인터페이스다. 테스트에 편리하도록 MockHttpServletResponse 이 제공된다.
+     */
+    @Test
+    void sessionTest() {
+        //세션 생성
+        MockHttpServletResponse response = new MockHttpServletResponse();
+        Member member = new Member();
+        sessionManager.createSession(member, response);
+
+        //요청에 응답 쿠키 저장
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.setCookies(response.getCookies());
+
+        //세션 조회
+        Object result = sessionManager.getSession(request);
+        assertThat(result).isEqualTo(member);
+
+        //세션 만료
+        sessionManager.expire(request);
+        Object expired = sessionManager.getSession(request);
+        assertThat(expired).isNull();
+    }
+}
